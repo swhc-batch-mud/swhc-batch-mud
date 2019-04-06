7cda97ee1e59b5c7bf1012aa91f449ddc618ecbe
Avi Kelman (fiendish) <patcherton.fixesthings@gmail.com>
2019-04-06 18:35:39 -0400

    add license and copyright notice

--- SWHC_BATCH_MUD.bat
+++ SWHC_BATCH_MUD.bat
@@ -1,4 +1,19 @@
 @ECHO OFF
+REM     Copyright (C) 2019  BRYCE NEUBERGER - /u/brycen64 @ Reddit
+REM     
+REM     This program is free software: you can redistribute it and/or modify
+REM     it under the terms of the GNU General Public License as published by
+REM     the Free Software Foundation, either version 3 of the License, or
+REM     (at your option) any later version.
+REM     
+REM     This program is distributed in the hope that it will be useful,
+REM     but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM     GNU General Public License for more details.
+REM     
+REM     You should have received a copy of the GNU General Public License
+REM     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+REM 
 REM BFCPEOPTIONSTART
 REM Advanced BAT to EXE Converter www.BatToExeConverter.com
 REM BFCPEEXE=
